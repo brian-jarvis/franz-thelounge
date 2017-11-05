48f8f73bc70f373e4b93b5832bcad422fc84bf42
Jonathan Juursema <jonathanjuursema@users.noreply.github.com>
2017-11-05 18:53:13 +0100

    Fix spacing.

--- webview.js
+++ webview.js
@@ -1,17 +1,13 @@
 module.exports = Franz => {
-  const getMessages = function getMessages() {
-    let indirectElements = document.querySelectorAll('.badge:not(.highlight)');
-    let direct = document.querySelectorAll('.badge.highlight').length;
-	let indirect = 0;
-
-	for (let i = 0; i < indirectElements.length; i += 1) {
-		if (indirectElements[i].innerHTML.length > 0) indirect++;
-    }
-	
-	console.log(direct, indirect);
-	
-    Franz.setBadge(direct, indirect);
-  };
-
-  Franz.loop(getMessages);
+	const getMessages = function getMessages() {
+		let indirectElements = document.querySelectorAll('.badge:not(.highlight)');
+		let direct = document.querySelectorAll('.badge.highlight').length;
+		let indirect = 0;
+		for (let i = 0; i < indirectElements.length; i += 1) {
+			if (indirectElements[i].innerHTML.length > 0) indirect++;
+		}
+		console.log(direct, indirect);
+		Franz.setBadge(direct, indirect);
+	};
+	Franz.loop(getMessages);
 };
