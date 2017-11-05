51be06917ead661f7cea940e53c6825f2cb9a232
Jonathan Juursema <github@jonathanj.nl>
2017-11-05 18:51:18 +0100

    Updated webview.js to reflect message counting like other recipes do it.
    E.g. the number of 'unreads' should not reflect to total amount of messages but instead the total amount of 'channels' that are marked unread.

--- webview.js
+++ webview.js
@@ -1,23 +1,17 @@
 module.exports = Franz => {
   const getMessages = function getMessages() {
-    let directElements = document.querySelectorAll('.badge.highlight');
     let indirectElements = document.querySelectorAll('.badge:not(.highlight)');
-    let direct = 0;
+    let direct = document.querySelectorAll('.badge.highlight').length;
 	let indirect = 0;
 
-    for (let i = 0; i < directElements.length; i += 1) {
-		n = parseInt(directElements[i].innerHTML);
-		direct += isNaN(n) ? 0 : n;
-    }
 	for (let i = 0; i < indirectElements.length; i += 1) {
-		n = parseInt(indirectElements[i].innerHTML);
-		indirect += isNaN(n) ? 0 : n;
+		if (indirectElements[i].innerHTML.length > 0) indirect++;
     }
 	
-    // Set Franz badge
+	console.log(direct, indirect);
+	
     Franz.setBadge(direct, indirect);
   };
 
-  // Check for new messages every second and update Franz badge
   Franz.loop(getMessages);
 };
